7ff0bf5883da82d7af223552fc8d4ab6284263dc
Perplexe45 <aasselin@free.fr>
2023-02-22 15:39:55 +0100

    MAJ du projet

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,15 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class RegisterController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+    }
+   
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
-    {
-        return $this->render('register/index.html.twig');
+
+    public function index(Request $request,UserPasswordHasherInterface $encodeur): Response //Injection de dépendance avec l'objet "Request"
+    {																				//On embarque l'objet "Request" dans la public function "index"
+          $user = new \App\Entity\User();				//L'instance de  l'objet Request sera chargé dans "$request'
+          $form = $this->createForm(RegisterType::class,$user);
+
+        $form->handleRequest($request); //Ecoute de la requête entrante avec "handleResquest" s'il y a un post 
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $user = $form->getData(); //Injectionde toutes les données du formulaire de "RegisterType.php"
+            $password = $user->getPassword(); //Récup du setter dans entité "User.php"
+
+            //appeler la méthode hashPassword() sur l'objet $encodeur (qui est une instance de UserPasswordHasherInterface)
+            // en lui passant l'objet User et le mot de passe en clair à hasher. La méthode hashPassword() retourne
+            // le mot de passe hashé, qui est ensuite définit sur la propriété password de l'objet User.
+            // Cela extrait le mot de passe en clair à partir de l'objet User et le hashe avec
+            // l'objet $encodeur et de définir le mot de passe hashé sur la propriété password de l'objet User.
+            $hashedPassword = $encodeur->hashPassword($user, $password);
+            $user->setPassword($hashedPassword);
+            
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
+        
+        return $this->render('register/index.html.twig', [
+            'form'=>$form->createView()
+        ]);
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,17 +4,63 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
+            ->add('firstName',TextType::class,[
+                'label' => 'Prénom',
+                'constraints' => [new Length(['min' => 4, 'max' => 30])],
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre prénom'
+                ]
+            ])
+            ->add('lastName',TextType::class,[
+                'label' => 'Nom',
+                'constraints' => [new Length(['min' => 4, 'max' => 30])],
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre Nom'
+                ]
+            ])
+            ->add('email',EmailType::class,[
+                'label' => 'Email',
+                'constraints' => [new Length(['min' => 5, 'max' => 40])],
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre Email'
+                ]
+            ])
+         
+            ->add('password', RepeatedType::class,[ //Les 2 champs pour le MDP et La vérif du MDP
+                'type' => PasswordType::class,      //sont gérés
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identiques',
+                'label' => 'Votre mot de passe',
+                'required'=> true,
+                'first_options'  => ['label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre mot de passe'
+                ]],
+                'second_options' => ['label' => 'Confirmez votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'Merci de confirmer votre mot de passe'
+                ]],
+                'constraints' => [new Length(['min' => 4, 'max' => 20])]
+                
+            ])
+						           
+		    ->add('submit', SubmitType::class,[
+                'label' => "S'inscrire"  
+            ])
         ;
     }
 
